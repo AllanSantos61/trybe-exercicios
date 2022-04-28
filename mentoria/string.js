b5438ea21a5d0761fce95350531d2012cf5e0971
Allan Santos <61allansantos@gmail.com>
2022-04-28 16:33:24 -0300

    pastas

--- mentoria/string.js
+++
@@ -1,9 +0,0 @@
-/*  Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word. 
-
-Valor de teste: 'trybe' e 'be'
-valor esperado no retorno da função: true
-Dica: Use o SPLIT. */
-
-function string(word,ending) {
-    
-}
